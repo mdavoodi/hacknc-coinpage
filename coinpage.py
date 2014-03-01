07b2e89aa892f68e59c5f527007b7e19ee212ac7
Mohammed <mdavoodi@vt.edu>
2014-03-01 16:14:25 -0500

    Added blank index page

--- coinpage.py
+++ coinpage.py
@@ -92,21 +92,8 @@ def before_request():
 
 
 @app.route('/')
-def timeline():
-    """Shows a users timeline or if no user is logged in it will
-    redirect to the public timeline.  This timeline shows the user's
-    messages as well as all the messages of followed users.
-    """
-    if not g.user:
-        return redirect(url_for('public_timeline'))
-    return render_template('timeline.html', messages=query_db('''
-        select message.*, user.* from message, user
-        where message.author_id = user.user_id and (
-            user.user_id = ? or
-            user.user_id in (select whom_id from follower
-                                    where who_id = ?))
-        order by message.pub_date desc limit ?''',
-        [session['user_id'], session['user_id'], PER_PAGE]))
+def index():
+    return render_template('index.html')
 
 
 @app.route('/public')
