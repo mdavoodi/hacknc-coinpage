80421683958603b7d823eafe7c1ae40c764675a7
mdavoodi <mohammeddavoodi@hotmail.com>
2014-03-01 19:35:50 -0500

    Template rework

--- coinpage.py
+++ coinpage.py
@@ -90,6 +90,9 @@ def before_request():
 def index():
     return render_template('index.html')
 
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
 
 @app.route('/public')
 def public_timeline():
@@ -192,31 +195,7 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Registers the user."""
-    if g.user:
-        return redirect(url_for('timeline'))
-    error = None
-    if request.method == 'POST':
-        if not request.form['username']:
-            error = 'You have to enter a username'
-        elif not request.form['email'] or \
-                 '@' not in request.form['email']:
-            error = 'You have to enter a valid email address'
-        elif not request.form['password']:
-            error = 'You have to enter a password'
-        elif request.form['password'] != request.form['password2']:
-            error = 'The two passwords do not match'
-        elif get_user_id(request.form['username']) is not None:
-            error = 'The username is already taken'
-        else:
-            db = get_db()
-            db.execute('''insert into user (
-              username, email, pw_hash) values (?, ?, ?)''',
-              [request.form['username'], request.form['email'],
-               generate_password_hash(request.form['password'])])
-            db.commit()
-            flash('You were successfully registered and can login now')
-            return redirect(url_for('login'))
-    return render_template('register.html', error=error)
+    return render_template('register.html')
 
 
 @app.route('/logout')
